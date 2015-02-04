f3dcade21f201999f72ae4084d3955f4ab6aef08
Simone Vittori <hello@simonewebdesign.it>
2015-02-04 18:24:36 +0000

    Date range

--- dist/tags/molecules/date-range/date-range.js
+++ dist/tags/molecules/date-range/date-range.js
@@ -1,8 +1,13 @@
-riot.tag('date-range', '<input id="startDate" type="date" onchange="{ updateMinEndDate }"> <input id="endDate" type="date" min="{ start }" max="2015-03-02">', function(opts) {
+riot.tag('date-range', '<input id="startDate" type="date" max="{ end }" onchange="{ updateMinEndDate }"> <input id="endDate" type="date" min="{ start }" max="{ opts.max }" onchange="{ updateMaxStartDate }">', function(opts) {
+
+  this.opts.max = "2015-03-02"
 
   this.updateMinEndDate = function(e) {
-    console.log("[date-range.js] updateMinEndDate() - event: ", e)
     this.start = this.startDate.value
+  }.bind(this);
+
+  this.updateMaxStartDate = function(e) {
+    this.end = this.endDate.value
   }.bind(this);
 
 
